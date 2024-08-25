d83a06ecd7868868e400987eeaf24cf377e29e36
Andrés Gallardo <andresgallardo95@gmail.com>
2024-08-25 17:52:42 -0400

    Separando ingredientes en un nuevo codigo

---
+++ ingredientes.py
@@ -0,0 +1,5 @@
+# ingredientes.py
+
+ingredientes_proteicos_posibles = ["pollo", "vacuno", "carne vegetal"]
+ingredientes_vegetales_posibles = ["tomate", "aceitunas", "champiñones"]
+tipos_de_masa_posibles = ["tradicional", "delgada"]
